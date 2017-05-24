97bbce59430febc62d4346bdbe4450b6f788cea4
Dereje <dergkat@yahoo.ca>
2017-05-24 19:16:27 +0300

    modularized

--- EST/preprocess.py
+++ EST/preprocess.py
@@ -11,17 +11,6 @@ def stft(x,Chunk_Size,overlap=1):
 	w = scipy.hanning(Chunk_Size+1)[:-1]
 	cnt = 0
 	return np.array([np.fft.rfft(w*x[i:i+Chunk_Size]) for i in range (0,len(x)-Chunk_Size,hop)])
-def root_mean_square(x,Chunk_Size,fs):
-	num_blocks = int(np.ceil(len(x)/Chunk_Size))
-	timestamps = (np.arange(0,num_blocks -1)* (Chunk_Size/float(fs)))
-	rms = []
-	for i in range(0,num_blocks-1):
-		start = i*Chunk_Size
-		stop = np.min([(start + Chunk_Size -1),len(x)])
-		rms_seg = np.sqrt(np.mean(x[start:stop]**2))
-		rms.append(rms_seg)
-	#---This is a computation for the RMS values, This will help in identifying which blocks are used for inflection----#
-	return np.nan_to_num(np.asarray(rms))
 def wave_file_read(filename):
 	fs,x = wavfile.read(filename)
 	return fs , x
@@ -48,27 +37,27 @@ def potential_inflection_fundamental_frequency(Fundamental_Frequency_of_Voiced_S
 	return inflect_frequency
 
 def matrix_of_sample_numbers(RMS,inflection_voices_samples):     	
-	InSamp = []
+	infelct_sample = []
 	for i in range(0,len(inflection_voices_samples)-1):
 	  	if (RMS[i]<np.mean(RMS) or (RMS[i]==0)):
-	 		InSamp.append(inflection_voices_samples[i])
-	# [InSamp] This matrix contains sample numbers for a difference 
+	 		infelct_sample.append(inflection_voices_samples[i])
+	# [infelct_sample] This matrix contains sample numbers for a difference 
 	# of voiced samples that are less than the mean of the RMS values. 
 	# According to D.A.V.I.D, this samples are recorded as attack  
 	# Since we will be applying inflection on new utterance,
 	# This will help for classifications amongst voiced samples.  
-	return np.asarray(InSamp)
-def consecutive_blocks_for_inflection(InSamp,Conblocks):
+	return np.asarray(infelct_sample)
+def consecutive_blocks_for_inflection(infelct_sample,Conblocks):
 	#---"inflect_blocks" is the consecutive blocks for inflection----#
 	inflect_blocks =[]
-	for i in range(len(InSamp)):
-		inflect_blocks.append(InSamp[i] + np.arange(Conblocks))
+	for i in range(len(infelct_sample)):
+		inflect_blocks.append(infelct_sample[i] + np.arange(Conblocks))
 	inflect_blocks = np.array(inflect_blocks)		 
 	return inflect_blocks
-def alteration_of_discrete_data(InSamp,Conblocks,inflect_blocks):
+def alteration_of_discrete_data(infelct_sample,Conblocks,inflect_blocks):
 	
 	selected_inflect_block = []
-	for i in range(len(InSamp)):
+	for i in range(len(infelct_sample)):
 		if (np.abs(inflect_blocks[i][0] - inflect_blocks[i-1][0]))> 2*(Conblocks-1):
 			selected_inflect_block.append(inflect_blocks[i])
 	selected_inflect_block = np.array(selected_inflect_block)
@@ -87,15 +76,15 @@ def consecutive_blocks_in_selected_blocks(selected_inflect_block,Conblocks):
 	# Are blocks that can be  altered with Pitch Up followed by Pitch Down then resolve to normal.
 	return n
 def reshaped_inflection_blocks(n,selected_inflect_block,Conblocks):
-	Inflection_Block_Samples = (np.reshape(selected_inflect_block,(n,Conblocks/3))).flatten()
-	# Inflection_Block_Samples is the reshaped Inflection Blocks for the purpose of categorizing blocks for Inflection 
-	# Based on 'n' The Inflection_Block_Samples is used for the classification of:  
-	return Inflection_Block_Samples
-def difference_arrays(num_blocks,Inflection_Block_Samples):
+	inflection_block_samples = (np.reshape(selected_inflect_block,(n,Conblocks/3))).flatten()
+	# inflection_block_samples is the reshaped Inflection Blocks for the purpose of categorizing blocks for Inflection 
+	# Based on 'n' The inflection_block_samples is used for the classification of:  
+	return inflection_block_samples
+def difference_arrays(num_blocks,inflection_block_samples):
 	A = np.array(np.arange(num_blocks))
-	B = np.array(Inflection_Block_Samples.flatten())
-	DiffArrays = np.array(list(set(A)-set(B)))
-	return DiffArrays
+	B = np.array(inflection_block_samples.flatten())
+	difference_arrays = np.array(list(set(A)-set(B)))
+	return difference_arrays
 
 def selected_inflect_block_new(inflection_sample_numbers):
 	selected_inflect_block_new = []
--- EST/synthesis.py
+++ EST/synthesis.py
@@ -3,23 +3,31 @@ import numpy as np
 from sox.transform import Transformer
 FILE_NAME_PATH = "/home/dereje/Desktop/TestFolder/Test.wav"
 CUTFREQ = 4000
+QFACTOR = 1
+PARAMETER_CONTROL = 1
+def appended_utterance_time_stamps(CONSECUTIVE_BLOCKS,TIME_STAMPS,selected_inflect_block):
+	utterance_time_stamps = []
+	for i in range(len(selected_inflect_block)):
+		utterance_time_stamps.append(TIME_STAMPS[selected_inflect_block[i][:CONSECUTIVE_BLOCKS]])
+	utterance_time_stamps = np.asarray(utterance_time_stamps)
+	return utterance_time_stamps
 def happy_inflection_function(normalized_time_stamps):
 	time = np.array([0.01,0.058511,0.255319,0.401596,0.500],ndmin=1)
-	fixed_cents = np.array([-200,140,-57.333,0.001,0.001],ndmin=1)
+	fixed_cents = np.array([0.001,140,82.667,-82.667,0.001],ndmin=1)
 	func = np.polyfit(time,fixed_cents,4)
 	inflect_func = np.poly1d(func)
 	cents = inflect_func(normalized_time_stamps)
 	return cents.tolist()
 def afraid_inflection_function(normalized_time_stamps):
 	time = np.array([0.01,0.058511,0.255319,0.401596,0.500],ndmin=1)
-	fixed_cents = np.array([120,-200,208,192,-192],ndmin=1)
+	fixed_cents = np.array([120,0.01,8,192,-192],ndmin=1)
 	func = np.polyfit(time,fixed_cents,4)
 	inflect_func = np.poly1d(func)
 	cents = inflect_func(normalized_time_stamps)
 	return cents.tolist()
 def happy_tensed_inflection_function(normalized_time_stamps):
 	time = np.array([0.01,0.058511,0.255319,0.401596,0.500],ndmin=1)
-	fixed_cents = np.array([-200,0.001,82.667,-82.667,0.001],ndmin=1)
+	fixed_cents = np.array([0.001,0.001,82.667,-82.667,0.001],ndmin=1)
 	func = np.polyfit(time,fixed_cents,4)
 	inflect_func = np.poly1d(func)
 	cents = inflect_func(normalized_time_stamps)
@@ -66,12 +74,12 @@ def concatenate_list(start_time_now,end_time_now):
 	start_time_now = start_time_now.tolist()
 	end_time_now = end_time_now.tolist()
 	return start_time_now,end_time_now
-def sox_init(filenameout,n_semitones,number_of_bends,start_time_now,end_time_now,cents,CUTFREQ,Gain,Qfactor):
+def happy_sox_init(filenameout,semitones,number_of_bends,start_time_now,end_time_now,cents,CUTFREQ,gain,QFACTOR):
 	patch = Transformer()
-	patch.pitch(n_semitones,False)
+	patch.pitch(semitones,False)
 	patch.bend(number_of_bends,start_time_now,end_time_now,cents,50)			
-	patch.treble(Gain,CUTFREQ,0.5)
-	patch.equalizer(CUTFREQ,Qfactor,Gain)
+	patch.treble(gain,CUTFREQ,0.5)
+	patch.equalizer(CUTFREQ,QFACTOR,gain)
 	patch.build(FILE_NAME_PATH,filenameout)
 	return patch
 def afraid_sox_init(speed,depth,number_of_bends,start_time_now,end_time_now,cents,filenameout):
@@ -80,42 +88,50 @@ def afraid_sox_init(speed,depth,number_of_bends,start_time_now,end_time_now,cent
 	patch.bend(number_of_bends,start_time_now,end_time_now,cents,50)			
 	patch.build(FILE_NAME_PATH,filenameout)
 	return patch
-def sad_sox_init(n_semitones,Gain,CUTFREQ,filenameout):
+def sad_sox_init(semitones,gain,CUTFREQ,filenameout):
 	CUTFREQ = 3500
 	patch = Transformer()
-	patch.pitch(n_semitones,False)
-	patch.treble(Gain,CUTFREQ,0.5)	
+	patch.pitch(semitones,False)
+	patch.treble(gain,CUTFREQ,0.5)	
 	patch.build(FILE_NAME_PATH,filenameout)
 	return patch
 
 
-def happy_patch(sampleFrequency,n_semitones,Qfactor,Gain,utterance_begin):
+def happy_patch(sampleFrequency,utterance_begin):
 	filenameout = "/home/dereje/Desktop/TestFolder/TestHappy.wav"
+	gain = 3.0
+	semitones = 1.0 * PARAMETER_CONTROL
 	start_time_now,end_time_now=start_end_times(utterance_begin)
 	cents = happy_cents_for_utterance(start_time_now)
 	start_time_now,end_time_now=concatenate_list(start_time_now,end_time_now)
 	number_of_bends = len(start_time_now)
-	happy_patch = sox_init(filenameout,n_semitones,number_of_bends,start_time_now,end_time_now,cents,CUTFREQ,Gain,Qfactor)
+	happy_patch = happy_sox_init(filenameout,semitones,number_of_bends,start_time_now,end_time_now,cents,CUTFREQ,gain,QFACTOR)
 	return happy_patch
 
-def happy_tensed_patch(sampleFrequency,n_semitones,Qfactor,Gain,utterance_begin):
+def happy_tensed_patch(sampleFrequency,utterance_begin):
 	filenameout = "/home/dereje/Desktop/TestFolder/TestTensedHappy.wav"
+	gain = 3.0
+	semitones = 2.0 * PARAMETER_CONTROL
 	start_time_now,end_time_now=start_end_times(utterance_begin)
 	cents = happy_tensed_cents_for_utterance(start_time_now)
 	start_time_now,end_time_now=concatenate_list(start_time_now,end_time_now)
 	number_of_bends = len(start_time_now)
-	happy_tensed_patch = sox_init(filenameout,n_semitones,number_of_bends,start_time_now,end_time_now,cents,CUTFREQ,Gain,Qfactor)
+	happy_tensed_patch = happy_sox_init(filenameout,semitones,number_of_bends,start_time_now,end_time_now,cents,CUTFREQ,gain,QFACTOR)
 	return happy_tensed_patch
 
 
-def sad_patch(sampleFrequency,n_semitones,Qfactor,Gain):
+def sad_patch(sampleFrequency):
 	filenameout = "/home/dereje/Desktop/TestFolder/TestSad.wav"
-	sad_patch = sad_sox_init(n_semitones,Gain,CUTFREQ,filenameout)
+	gain = 0.25
+	semitones = -1.5 * PARAMETER_CONTROL
+	sad_patch = sad_sox_init(semitones,gain,CUTFREQ,filenameout)
 	return sad_patch
 
 
-def afraid_patch(sampleFrequency,speed,depth,utterance_begin):
+def afraid_patch(sampleFrequency,utterance_begin):
 	filenameout = "/home/dereje/Desktop/TestFolder/TestAfraid.wav"
+	speed = 8.5
+	depth = 1 + (60 * PARAMETER_CONTROL)
 	start_time_now,end_time_now=start_end_times(utterance_begin)
 	cents = afraid_cents_for_utterance(start_time_now)
 	start_time_now,end_time_now = concatenate_list(start_time_now,end_time_now)
